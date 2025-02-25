509ad89ca4afd3ece371ce2cda228c4ee5532aba
Samero0 <alex.fr.estudio@gmail.com>
2025-02-25 11:47:14 +0000

    added winner rule

--- src/App.js
+++ src/App.js
@@ -17,9 +17,17 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Ganador: " + winner;
+  } else {
+    status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
 
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)) {
       return;  //si el lugar está ocupado, no se hace nada
     }
 
@@ -35,6 +43,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -54,5 +63,23 @@ export default function Board() {
   );
 }
 
-
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
